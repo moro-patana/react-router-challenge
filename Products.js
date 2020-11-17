3d50909c5360802108b21614a6736a5b4d81b94a
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-17 09:16:59 +0300

    map through the data products

--- Products.js
+++ Products.js
@@ -5,10 +5,12 @@ console.log(ProductsData);
 function Products() {
 	return (
 		<div>
+			<h1>Products</h1>
               {ProductsData.map(product => (
-				  <div>
+				  <div key={product.id}>
 					  <h2>{product.name}</h2>
-	  				  <p>{product.description} <span>${product.price}</span></p>
+	  				  <p>{product.description} <span>Price: ${product.price}</span></p>
+				      <hr/>
 				  </div>
 			  ))}
 		</div>
